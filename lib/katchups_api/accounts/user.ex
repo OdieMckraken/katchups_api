f1121bf2e17657d84e1de35ccdc784a96b240847
Adam Workman <adammworkman25@gmail.com>
2022-05-30 14:36:02 -0400

    implement user model validation

--- lib/katchups_api/accounts/user.ex
+++ lib/katchups_api/accounts/user.ex
@@ -9,7 +9,10 @@ defmodule KatchupsApi.Accounts.User do
     field :favorite_restaurants, {:array, :string}
     field :first_name, :string
     field :last_name, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
     field :password_hash, :string
+    field :role, :string, default: "user" 
 
     timestamps()
   end
@@ -21,7 +24,8 @@ defmodule KatchupsApi.Accounts.User do
       :first_name,
       :last_name,
       :email,
-      :password_hash,
+      :password,
+      :password_confirmation,
       :avatar,
       :favorite_friends,
       :favorite_restaurants
@@ -30,10 +34,19 @@ defmodule KatchupsApi.Accounts.User do
       :first_name,
       :last_name,
       :email,
-      :password_hash,
-      :avatar,
-      :favorite_friends,
-      :favorite_restaurants
-    ])
+      :password,
+      :password_confirmation
+      :avatar
+    ]) |> unique_constraint([
+    :email
+    ]) |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 8, max: 100)
+    |> validate_confirmation(:passowrd)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
