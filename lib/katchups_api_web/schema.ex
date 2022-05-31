7b5b23a990287c3dc14c5a54825e9899c0762e26
Adam Workman <adammworkman25@gmail.com>
2022-05-30 21:40:09 -0400

    add query and mutation

--- lib/katchups_api_web/schema.ex
+++ lib/katchups_api_web/schema.ex
@@ -1,19 +1,23 @@
 defmodule KatchupsApiWeb.Schema do
   use Absinthe.Schema
-
+  alias KatchupsApiWeb.Resolvers
   # import types
-  import_types(KatchupsGraphqlApiWeb.Schema.Types)
+  import_types(KatchupsApiWeb.Schema.Types)
 
   query do
     @desc "Get all users"
     field :users, list_of(:user_type) do
-      # resolver
       resolve(&Resolvers.UserResolver.users/3)
     end
   end
 
-  # mutate do
-  # end
+  mutation do
+    @desc "Register a new user"
+    field :register_user, type: :user_type do
+      arg(:input, non_null(:user_input_type))
+      resolve(&Resolvers.UserResolver.register_user/3)
+    end
+  end
 
   # subscription do
   # end
