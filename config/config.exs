6b87ecd1a91a16edb0b8a567cb4f629002f5f841
Adam Workman <adammworkman25@gmail.com>
2022-06-01 18:45:54 -0400

    add guardian config details

--- config/config.exs
+++ config/config.exs
@@ -37,6 +37,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Guardian config details
+config :katchups_api, KatchupsApi.Guardian,
+       issuer: "katchups_api",
+       secret_key: "wNAP5AZ2AeIfpWNsnb6H4zLuhvVIhJ2puPMdc//7OjfsXnZmf+E2eIolncSaJiV+"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
